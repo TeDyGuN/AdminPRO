566aa42d460d58e3092510889ed8e8b5b79dda36
Ted Carrasco <ted_cc@hotmail.com>
2018-04-23 14:02:00 -0400

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,27 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Modulos
+import { PagesModule } from './pages/pages.module';
 
+import { APP_ROUTES } from './app.route';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { GraficaComponent } from './pages/grafica/grafica.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { APP_ROUTES } from './app.route';
+import { RegisterComponent } from './login/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    GraficaComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,17 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { GraficaComponent } from './pages/grafica/grafica.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { RegisterComponent } from './login/register.component';
 
 const appRoutes: Routes = [
-    {path: 'dashboard', component: DashboardComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'register', component: LoginComponent},
-    {path: 'progress', component: ProgressComponent},
-    {path: 'grafica', component: GraficaComponent},
-    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    {path: 'register', component: RegisterComponent},
     {path: '**', component: NopagefoundComponent}
 ];
 
